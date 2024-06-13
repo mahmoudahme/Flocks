98d444864794dd288c2874c1396a676109b3d876
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-06-14 02:45:43 +0300

    Update authRouter.js

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {register , login } from "../controller/authController.js";
+import { verifyToken } from "../Utils/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/register", register)
 router.post("/login" , login)
+router.post("/logout", verifyToken ,logout)
 
 
 export default router
