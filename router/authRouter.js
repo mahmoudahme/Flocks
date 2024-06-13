a91281f8dcb77720957b6e38a97756773066d253
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-06-14 02:51:07 +0300

    Update authRouter.js

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {register , login } from "../controller/authController.js";
+import {register , login , logout} from "../controller/authController.js";
 import { verifyToken } from "../Utils/verifyToken.js";
 
 const router = express.Router();
