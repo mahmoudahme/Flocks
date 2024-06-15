e0037da59b60487c5886bcbe635a783779c84f9c
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-06-15 04:12:15 +0300

    Update feedServiedRouter.js

--- router/feedServiedRouter.js
+++ router/feedServiedRouter.js
@@ -1,11 +1,12 @@
 import  express  from "express";
-import { createFeedServied, getFeedServiedForFlocks } from "../controller/feedServiedController.js";
+import { createFeedServied, getFeedServiedForFlocks , deleteFeedServied } from "../controller/feedServiedController.js";
 
 const router = express.Router();
 
 
 router.get("/:flockiD", getFeedServiedForFlocks )
 router.post("/create/:id" ,createFeedServied );
+router.delete("/delete/:flockiD/:feedServedID" , deleteFeedServied );
 
 
 export default router ; 
