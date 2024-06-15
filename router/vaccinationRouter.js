9e7285b86e84bd96ed3b614180500cc24a0a5602
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-06-15 03:42:12 +0300

    Update vaccinationRouter.js

--- router/vaccinationRouter.js
+++ router/vaccinationRouter.js
@@ -1,11 +1,11 @@
 import  express  from "express";
-import {createVaccination , getVaccinationForFlocks  } from "../controller/vaccinationController.js";
+import {createVaccination , getVaccinationForFlocks , deleteVaccination } from "../controller/vaccinationController.js";
 
 const router = express.Router();
 
 
 router.get("/:flockiD", getVaccinationForFlocks)
 router.post("/create/:id" , createVaccination);
-
+router.delete("/delete/:flockiD/:VaccinationID" , deleteVaccination );
 
 export default router ; 
