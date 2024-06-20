f61512cf3724639d4b32e274ad79bd7a4d35f489
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-06-20 12:42:58 +0300

    Create incomeFinController.js

---
+++ controller/incomeFinController.js
@@ -0,0 +1,28 @@
+import income from "../model/income.js";
+import {ApiError} from "../Utils/apiError.js";
+import { verifyToken } from "../Utils/verifyToken.js";
+import dotenv from "dotenv" ;
+
+dotenv.config({path : 'config/config.env'});
+
+export const InconmeFin = async(req , res , next)=>{
+    try {
+        const flockiD = req.params.flockiD ;
+        verifyToken(req , res , async()=>{
+            if(req.user){
+                const Incomes = await income.find({flockiD : flockiD})
+                var fini = 0; 
+                for(var i = 0 ; i < Incomes.length ; i ++){
+                    fini += Incomes[i].Amount ;
+                }
+                res.status(200).json({Incomes : Incomes})
+                
+            }else{
+                return next(new ApiError(`You are not user` , 404))
+                
+            } 
+        })
+    } catch (error) {
+        return next(new ApiError(`System Error ${error}` , 404))
+    }
+}
