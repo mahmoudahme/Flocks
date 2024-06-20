c2acfd8ff6969cbdcb3b872e3071c5e2246415d9
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-06-20 11:57:09 +0300

    Update medicianController.js

--- controller/medicianController.js
+++ controller/medicianController.js
@@ -1,4 +1,5 @@
 import Medician from "../model/medician.js";
+import consumptionInventory from "../model/consumptionInventory.js";
 import {ApiError} from "../Utils/apiError.js";
 import { verifyToken } from "../Utils/verifyToken.js";
 import dotenv from "dotenv" ;
@@ -9,19 +10,35 @@ export const createMedician = async(req , res , next )=>{
     try {
         verifyToken(req , res , async()=>{
             if(req.user){
-                console.log(req.body)
-                const newmedician = new Medician({
-                Name :req.body.Name ,
-                breed : req.body.breed, 
-                Note: req.body.Note,
-                FlockID: req.params.id 
-            }) 
-            await newmedician.save();
-            res.status(200).json({message : "Medician Created"})
+                const conInventory = await consumptionInventory.findOne({Name : req.body.Name}) ;
+                if(conInventory){
+                    if(conInventory.Quantity >= req.body.Amount){
+                        const newmedician = new Medician({
+                        Name :req.body.Name ,
+                        breed : req.body.breed, 
+                        Note: req.body.Note,
+                        FlockID: req.params.id 
+                    }) 
+                        await newmedician.save();
+                        res.status(200).json({message : "Medician Created"})
+
+                     const Quantity2 = conInventory.Quantity2 ;
+                        await consumptionInventory.findByIdAndUpdate(
+                            conInventory.id, 
+                            { Quantity:conInventory.Quantity - req.body.Amount , 
+                              Prercent : ((conInventory.Quantity - req.body.Amount) /Quantity2)*100 
+                            },
+                            { new: true }
+                        )
+                    }else{
+                        res.status(200).json({message : "this Quantity isn't enough"})
+                    }
+                }else{
+                     res.status(200).json({message : "this name doesn't Exist"})
+                }
             }else{
                 return next(new ApiError(`You are not user` , 401))
             }
-            
         })
     } catch (error) {
         return next(new ApiError(`System Error ${error}` , 404))
