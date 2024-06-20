193fb016b0741d63ff19b8e89e00109ea6c9a2d7
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-06-20 12:18:19 +0300

    Update flockRouter.js

--- router/flockRouter.js
+++ router/flockRouter.js
@@ -1,6 +1,6 @@
 import  express  from "express";
 import {verifyAdmin ,verifyUser , verifyUserAndAdmin} from "../Utils/verifyToken.js"
-import {createFlocks, deleteFlocks, getFlockBid, getFlocks } from "../controller/flockController.js";
+import {createFlocks, deleteFlocks, getFlockBid, getFlocks ,  updateFlock} from "../controller/flockController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get("/" , getFlocks)
 router.get("/:id" , getFlockBid)
 router.post("/create" ,  createFlocks);
 router.delete("/delete/:id" ,  deleteFlocks);
+router.put("/:id" , updateFlock);
 
 export default router ; 
