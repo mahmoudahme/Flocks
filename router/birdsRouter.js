a8ade3846aca8163909ad2a4d8cc5411da8525f1
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-06-22 05:13:27 +0300

    Update birdsRouter.js

--- router/birdsRouter.js
+++ router/birdsRouter.js
@@ -1,9 +1,9 @@
 import  express  from "express";
-import {getBirdsOfFlocks , water} from "../controller/birdsController.js";
+import {getBirdsOfFlocks , water , food} from "../controller/birdsController.js";
 
 const router = express.Router();
 
 router.get("/" , getBirdsOfFlocks)
 router.get("/water" , water)
-
+router.get("/food" , food)
 export default router ; 
